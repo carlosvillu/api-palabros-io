cf03368a84e31bf13873c3fa66f55a9bd520f7bc
Carlos Villuendas Zambrana <carlosvillu@gmail.com>
2022-12-26 19:05:38 +0100

    feat(functions): add more filters

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,7 +4,13 @@ import {words} from "./words";
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
 export const search = functions.https.onRequest((request, response) => {
-  const query = request.query?.query as string ?? "";
+  const start = request.query?.start as string ?? "";
+  const ends = request.query?.ends as string ?? "";
+  const contains = request.query?.contains as string ?? "";
+  const length = parseInt(request.query?.length as string ?? 0, 10);
+  let query = request.query?.query as string ?? ".*";
+
+  query = query.toLowerCase();
   functions.logger.info("Search", {query});
 
   const pattern = query
@@ -12,9 +18,20 @@ export const search = functions.https.onRequest((request, response) => {
       .replaceAll(" ", ".");
 
   const matches = words.matchAll(new RegExp('\n' + pattern + '\n', 'g')) // eslint-disable-line 
-  const solution = [...matches]
+  let solution = [...matches]
       .map((match) => match[0])
       .map((match) => match.replaceAll("\n", ""));
+
+  if (length) solution = solution.filter((word) => word.length === length);
+  if (start) solution = solution.filter((word) => word.match(new RegExp(`^${start}`))); // eslint-disable-line 
+  if (ends) solution = solution.filter((word) => word.match(new RegExp(`${ends}$`))); // eslint-disable-line 
+  if (contains) {
+    const letters = contains.split("");
+    solution = letters.reduce((sol, letter) => {
+      sol = sol.filter((word) => word.match(new RegExp(letter)));
+      return sol;
+    }, solution);
+  }
 
   response
       .setHeader("Access-Control-Allow-Origin", "*")
